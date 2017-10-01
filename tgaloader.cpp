f1303e3887eb53f2a00959ccf8b29c2135d27543
Gregor Soennichsen <christian.liebmann@outlook.de>
2017-10-02 00:06:03 +0200

    1.1.0 - completed the tga loader

--- tgaloader.cpp
+++ tgaloader.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 #include "tgaloader.hpp"
 #include "helpfunctions.hpp"
@@ -17,6 +18,8 @@ using namespace std;
 
 
 void loadTGA(string filePath) {
+
+    cout << "File: " << filePath << endl;
 
     ifstream myFile(filePath);
 
@@ -35,12 +38,39 @@ void loadTGA(string filePath) {
     // Since only TGA-files with no Image-ID and no Pallette are supported,
     // here immediatly the image data can be read.
 
-    unsigned short bytesPerPoint = header.bitsPerPoint / 8;
-    unsigned long long imSize    = ((long long) header.width) * header.height * bytesPerPoint;
+    uint16_t bytesPerPoint = header.bitsPerPoint / 8;
+    unsigned long long imSize = ((long long) header.width) * header.height * bytesPerPoint;
 
-    unsigned char *imData = new unsigned char[imSize];
-    myFile.read((char *) imData, imSize);
-    delete[] imData;
+    uint8_t *imRawData = new uint8_t[imSize];
+    myFile.read((char *) imRawData, imSize);
+
+    // In TGA-images the origin is in the lower-right corner and the image has to
+    // be processed row by row from the bottom to the top. Here, pixel are stored,
+    // that the origin is in the lower-left corner.
+
+    vector<uint8_t> imData[header.height][header.width];
+
+    uint16_t h=0, w=0;
+    unsigned long long i;
+    for(i=0; i<imSize; i+=bytesPerPoint) {
+
+        vector<uint8_t> pixel;
+
+        uint8_t offset;
+        for(offset=0; offset<bytesPerPoint; offset++) {         // Depending on either 24- or 32-Bits per pixel,
+            pixel.push_back(imRawData[i+offset]);               // 3 or 4 bytes are read into the vector, which
+        }                                                       // represents the colors of a pixel.
+
+        imData[h][(header.width-1)-w] = pixel;
+
+        w++;
+        if(w == header.width) {
+            w = 0;
+            h++;
+        }
+    }
+
+    delete[] imRawData;
 
     // Further Meta-Data following the image data are ignored.
 
@@ -53,22 +83,22 @@ struct tgaHeader readTGAHeader(ifstream *myFile) {
 
     struct tgaHeader header;
 
-    myFile->read((char *) &header.imageIDlength, sizeof(unsigned char));
-    myFile->read((char *) &header.colormapType, sizeof(unsigned char));
-    myFile->read((char *) &header.imageType, sizeof(unsigned char));
+    myFile->read((char *) &header.imageIDlength, 1);
+    myFile->read((char *) &header.colormapType, 1);
+    myFile->read((char *) &header.imageType, 1);
 
-    myFile->read((char *) &header.colormapBegin, sizeof(short int));
-    myFile->read((char *) &header.colormapLength, sizeof(short int));
+    myFile->read((char *) &header.colormapBegin, 2);
+    myFile->read((char *) &header.colormapLength, 2);
 
-    myFile->read((char *) &header.sizeOfEntryInPallette, sizeof(unsigned char));
+    myFile->read((char *) &header.sizeOfEntryInPallette, 1);
 
-    myFile->read((char *) &header.xOrigin, sizeof(short int));
-    myFile->read((char *) &header.yOrigin, sizeof(short int));
-    myFile->read((char *) &header.width, sizeof(short int));
-    myFile->read((char *) &header.height, sizeof(short int));
+    myFile->read((char *) &header.xOrigin, 2);
+    myFile->read((char *) &header.yOrigin, 2);
+    myFile->read((char *) &header.width, 2);
+    myFile->read((char *) &header.height, 2);
 
-    myFile->read((char *) &header.bitsPerPoint, sizeof(unsigned char));
-    myFile->read((char *) &header.attributeByte, sizeof(unsigned char));
+    myFile->read((char *) &header.bitsPerPoint, 1);
+    myFile->read((char *) &header.attributeByte, 1);
 
     return header;
 
@@ -78,16 +108,16 @@ struct tgaHeader readTGAHeader(ifstream *myFile) {
 
 void printTGAHeader(struct tgaHeader *header) {
 
-    cout << "Image-ID length:\t" << (short int) header->imageIDlength << endl;
-    cout << "Color-Pallete type:\t" << (short int) header->colormapType << endl;
-    cout << "Imagetype:\t\t" << (short int) header->imageType << endl;
-    cout << "Pallette-Begin:\t\t" << header->colormapBegin << endl;
-    cout << "Pallette-Length:\t" << header->colormapLength << endl;
-    cout << "Size of an entry\nin the pallette:\t" << (short int) header->sizeOfEntryInPallette << endl;
-    cout << "x-Origin:\t\t" << header->xOrigin << endl;
-    cout << "y-Origin:\t\t" << header->yOrigin << endl;
-    cout << "Image width:\t\t" << header->width << endl;
-    cout << "Image height:\t\t" << header->height << endl;
-    cout << "Bits per point:\t\t" << (short int) header->bitsPerPoint << endl;
+    cout << "Image-ID length:\t"    << (short) header->imageIDlength << endl;
+    cout << "Color-Pallete type:\t" << (short) header->colormapType << endl;
+    cout << "Imagetype:\t\t"        << (short) header->imageType << endl;
+    cout << "Pallette-Begin:\t\t"   << header->colormapBegin << endl;
+    cout << "Pallette-Length:\t"    << header->colormapLength << endl;
+    cout << "Size of colormap-entry:\t"<< (short) header->sizeOfEntryInPallette << endl;
+    cout << "x-Origin:\t\t"         << header->xOrigin << endl;
+    cout << "y-Origin:\t\t"         << header->yOrigin << endl;
+    cout << "Image width:\t\t"      << header->width << endl;
+    cout << "Image height:\t\t"     << header->height << endl;
+    cout << "Bits per point:\t\t"   << (short) header->bitsPerPoint << endl;
 
 }
--- tgaloader.hpp
+++ tgaloader.hpp
@@ -10,25 +10,34 @@
 
 #include <string>
 #include <fstream>
+#include <vector>
+
 
 using namespace std;
 
 
 
+
+typedef vector<uint8_t> pixelRGB;
+typedef vector<uint8_t> pixelRGBA;
+
+
+
+// TODO: Change types to standard-width uint types from "http://www.cplusplus.com/reference/cstdint/"
 struct tgaHeader {
 
-    unsigned char imageIDlength;
-    unsigned char colormapType;
-    unsigned char imageType;
-    short int colormapBegin;
-    short int colormapLength;
-    unsigned char sizeOfEntryInPallette;
-    short int xOrigin;
-    short int yOrigin;
-    short int width;
-    short int height;
-    unsigned char bitsPerPoint;
-    unsigned char attributeByte;
+    uint8_t imageIDlength;
+    uint8_t colormapType;
+    uint8_t imageType;
+    uint16_t colormapBegin;
+    uint16_t colormapLength;
+    uint8_t  sizeOfEntryInPallette;
+    uint16_t xOrigin;
+    uint16_t yOrigin;
+    uint16_t width;
+    uint16_t height;
+    uint8_t bitsPerPoint;
+    uint8_t attributeByte;
 
 };
 
